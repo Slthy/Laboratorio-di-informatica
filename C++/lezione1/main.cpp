2876d9c70ffa87dbb5a9fcd11bb8b12191be94eb
Alessandro Borsato <alessandroborsato5@gmail.com>
2022-02-04 21:29:06 +0100

    slthy.github.io & C++

---
+++ C++/lezione1/main.cpp
@@ -0,0 +1,21 @@
+/*
+  lezione1
+  Borsato Alessandro
+  04/04/2022
+*/
+
+
+//"int" significa che la funzione main restituisce un valore numerico intero in output
+//"main" è il nome della funzione principale
+//il compilatore comincia l'esecuzione del programma dalla funzione "main"
+int main(){
+  /*
+  questo è un commento su più righe
+  */
+  //questo è un commento che vale solo fino alla fine della riga
+
+  return 0;
+  //restituisce il valore di uscita della funzione main
+  //'0' significa che l'esecuzione si è conclusa senza errori
+  //diverso da '0' il numero indica al Sistema Operativo che tipo di errrore si è verificato
+}
---
+++ C++/lezione1/main2.cpp
@@ -0,0 +1,14 @@
+/*
+  lezione1
+  Borsato Alessandro
+  04/04/2022
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout<<"Ciao mondo!"<<endl;
+  return 0;
+}
