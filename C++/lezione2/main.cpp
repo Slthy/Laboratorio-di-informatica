42bf4c0c39fd7d57ed581c7d0f11cd46bf490196
Alessandro Borsato <alessandroborsato5@gmail.com>
2022-03-04 21:44:56 +0100

    git fix

---
+++ C++/lezione2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  int x {10};
+  int y {20};
+  
+  cout<<"Il mio numero x è: "<<x<<endl;
+  cout<<"Il mio numero y è: "<<y<<endl;
+
+  x = 100;
+  y = 200;
+  
+  cout<<"Il mio numero x è: "<<x<<endl;
+  cout<<"Il mio numero y è: "<<y<<endl;
+
+  return 0;
+}
