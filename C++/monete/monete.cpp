974a07142cef321eb9c42e023283afd766b95c4c
Alessandro Borsato <alessandroborsato5@gmail.com>
2022-03-22 09:08:41 +0100

    refactoring

--- C++/monete/monete.cpp
+++ C++/monete/monete.cpp
@@ -21,21 +21,21 @@ void outputMonete(int e2, int e1, int c50, int c20, int c10, int c5, int c2, int
   return;
 }
 
-void calcolaMoneteModulo(int totale, int &e2, int &e1, int &c50, int &c20, int &c10, int &c5, int &c2, int &c1){ //algoritmo ricorsivo
+void calcolaMonete(int totale, int &e2, int &e1, int &c50, int &c20, int &c10, int &c5, int &c2, int &c1){ //algoritmo ricorsivo
   cout<<totale;
-  if ((totale - 200) >= 0) {e2++; calcolaMoneteModulo(totale - 200, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 100) >= 0) {e1++; calcolaMoneteModulo(totale - 100, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 50) >= 0) {c50++; calcolaMoneteModulo(totale - 50, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 20) >= 0) {c20++; calcolaMoneteModulo(totale - 20, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 10) >= 0) {c10++; calcolaMoneteModulo(totale - 10, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 5) >= 0) {c5++; calcolaMoneteModulo(totale - 5, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 2) >= 0) {c2++; calcolaMoneteModulo(totale - 2, e2, e1, c50, c20, c10, c5, c2, c1);}
-    else if ((totale - 1) >= 0) {c1++; calcolaMoneteModulo(totale - 1, e2, e1, c50, c20, c10, c5, c2, c1);}
+  if ((totale - 200) >= 0) {e2++; calcolaMonete(totale - 200, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 100) >= 0) {e1++; calcolaMonete(totale - 100, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 50) >= 0) {c50++; calcolaMonete(totale - 50, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 20) >= 0) {c20++; calcolaMonete(totale - 20, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 10) >= 0) {c10++; calcolaMonete(totale - 10, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 5) >= 0) {c5++; calcolaMonete(totale - 5, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 2) >= 0) {c2++; calcolaMonete(totale - 2, e2, e1, c50, c20, c10, c5, c2, c1);}
+    else if ((totale - 1) >= 0) {c1++; calcolaMonete(totale - 1, e2, e1, c50, c20, c10, c5, c2, c1);}
   outputMonete(e2,  e1,  c50,  c20,  c10,  c5,  c2,  c1);
   return;
 }
 
-void calcolaMonete(int totale){
+void calcolaMoneteModulo(int totale){
   int e2 = 0, e1 = 0, c50 = 0, c20 = 0, c10 = 0, c5 = 0, c2 = 0, c1 = 0;
   do{
     if ((totale % 200) == 0) {e2++; totale -= 200;}
@@ -63,18 +63,18 @@ int main(){
     switch (opzioneUtente.input){
       case 'y':{
         opzioneUtente.error = false;
-        calcolaMonete(safeTotale);
+        calcolaMoneteModulo(safeTotale);
         break;
       }
       case 'n':{
         opzioneUtente.error = false;
         int e2 = 0, e1 = 0, c50 = 0, c20 = 0, c10 = 0, c5 = 0, c2 = 0, c1 = 0;
-        calcolaMoneteModulo(safeTotale, e2, e1, c50, c20, c10, c5, c2, c1);
+        calcolaMonete(safeTotale, e2, e1, c50, c20, c10, c5, c2, c1);
         break;
       }
       default:{
-        opzioneUtente.error = false;
-        cout<<"error";
+        opzioneUtente.error = true;
+        cout<<endl;
         break;
       }
     }
